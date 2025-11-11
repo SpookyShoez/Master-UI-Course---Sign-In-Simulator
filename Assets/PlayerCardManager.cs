fa193adfe393c69f768cabe4fc8de695cc7ea945
Morialle Barr <Moriallekbarr@gmail.com>
2025-11-11 15:07:38 +0200

    Lessons 3-4

--- Assets/PlayerCardManager.cs
+++ Assets/PlayerCardManager.cs
@@ -6,8 +6,21 @@ using UnityEngine;
 public class PlayerCardManager : MonoBehaviour
 {
     public TMP_Text nameText;
+    public TMP_Text descriptionText;
+    public TMP_Text ageText;
+
     public void UpdateName(string newName)
     {
         nameText.text = newName;
+    }
+
+    public void UpdateDescription(string newDescription)
+    {
+        descriptionText.text = newDescription;
+    }
+
+    public void UpdateAge(float newValue)
+    {
+        ageText.text = newValue.ToString() + " years old";
     }
 }
